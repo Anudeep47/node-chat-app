055dce96e164708c53db3db1fc4a0011e1c5ac45
Anudeep Pala <anudeep479@gmail.com>
2018-05-17 17:02:29 +0530

    emit new message on create message

--- server/server.js
+++ server/server.js
@@ -14,14 +14,13 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
-  socket.emit('newMessage', {
-    from: 'Anudeep',
-    text: 'Helloo buddyy',
-    createdAt: 123
-  });
-
   socket.on('createMessage', (message) => {
     console.log('message to be created: ', message);
+    io.emit('newMessage', {
+      from: message.from,
+      text: message.text,
+      createdAt: new Date().getTime()
+    });
   });
 
   socket.on('disconnect', () => {
