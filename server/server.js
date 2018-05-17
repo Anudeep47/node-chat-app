2f10da64007cb1b36986889c839acab89259a461
Anudeep Pala <anudeep479@gmail.com>
2018-05-17 17:34:53 +0530

    welcome users and inform others

--- server/server.js
+++ server/server.js
@@ -14,13 +14,29 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
+  socket.emit('newMessage', {
+    from: 'Admin',
+    text: 'Welcome to the Chat App :)'
+  });
+
+  socket.broadcast.emit('newMessage', {
+    from: 'Admin',
+    text: 'Yayy, A new user has joined our Chat App :)'
+  });
+
   socket.on('createMessage', (message) => {
     console.log('message to be created: ', message);
-    io.emit('newMessage', {
-      from: message.from,
-      text: message.text,
-      createdAt: new Date().getTime()
-    });
+    // io.emit('newMessage', {
+    //   from: message.from,
+    //   text: message.text,
+    //   createdAt: new Date().getTime()
+    // });
+
+    // socket.broadcast.emit('newMessage', {
+    //   from: message.from,
+    //   text: message.text,
+    //   createdAt: new Date().getTime()
+    // });
   });
 
   socket.on('disconnect', () => {
